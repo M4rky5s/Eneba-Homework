efdb0a1fad12c2503355abcdcc9b77bd9c8acbed
Markas <markutis2255@gmail.com>
2026-01-14 01:39:03 +0200

    Fixed SearchBar

--- react-app/src/components/SearchBar.tsx
+++ react-app/src/components/SearchBar.tsx
@@ -2,6 +2,7 @@ import "./SearchBar.css";
 import { useRef } from "react";
 
 import { CiSearch } from "react-icons/ci";
+import { IoClose } from "react-icons/io5";
 
 type SearchBarProps = {
     value: string;
@@ -11,13 +12,22 @@ type SearchBarProps = {
 export default function SearchBar({ value, onChange}: SearchBarProps) {
     const inputRef = useRef<HTMLInputElement>(null);
 
-    const handleIconClick = () => {
+    const handleSearchIconClick = () => {
+        inputRef.current?.focus();
+    };
+
+    const handleClearIconClick = () => {
+        onChange("");
         inputRef.current?.focus();
     };
 
     return (
         <div className="input-wrapper">
-            <button type="button" onClick={handleIconClick}>
+            <button type="button"
+                    onClick={handleSearchIconClick}
+                    className="icon-btn"
+                    aria-label="Focus search"
+            >
                 <CiSearch id="search-icon" />
             </button>
             <input
@@ -26,6 +36,17 @@ export default function SearchBar({ value, onChange}: SearchBarProps) {
                 onChange={(e) => onChange(e.target.value)}
                 placeholder="Ieškoti žaidimų, papildymų ir daugiau"
             />
+
+            {value.trim().length > 0 && (
+                <button
+                    type="button"
+                    onClick={handleClearIconClick}
+                    className="clear-btn"
+                    aria-label="Clear search"
+                >
+                    <IoClose size={26}/>
+                </button>
+            )}
         </div>
     );
 }
